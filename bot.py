c5ce80cb9bff33f636e5befcb51c0803567f7f6f
losh14ka <76164035+losh14ka@users.noreply.github.com>
2025-08-21 13:24:34 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ ssl_context.verify_mode = ssl.CERT_NONE
 
 # Монети + лімітки
 CONTRACTS = {  # ф’ючерси
-    "K_USDT": 0.24       # ф’ючерси
+    "K_USDT": 0.228       # ф’ючерси
 }
 SPOT_TOKENS = {
     "SNEK_USDT": 0.0075    # спот
@@ -111,6 +111,7 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
+
 
 
 
